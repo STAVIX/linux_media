b8a0b55e5a8a3b128742ea6c66a7204488bae341
STAVIX <40598359+STAVIX@users.noreply.github.com>
2020-04-30 11:39:58 +0800

    Update stavix_dma.c

--- drivers/media/pci/stavix/stavix_dma.c
+++ drivers/media/pci/stavix/stavix_dma.c
@@ -142,6 +142,7 @@ static void replace_tasklet_schedule(struct stavix_dev *dev)
                         data =  data + 192;
                         }
                 else{
+			i = i + 1;
                         for(j = 0; j < 192; j++)
                         {
                                 if(data[0] <= 0x07)
@@ -149,7 +150,6 @@ static void replace_tasklet_schedule(struct stavix_dev *dev)
                                         data = data + 192;
                                         if(data[0] <= 0x07){
                                           data = data - 192;
-                                          i = i + 1;
                                           break;
                                         }else{
                                           data = data - 192;
